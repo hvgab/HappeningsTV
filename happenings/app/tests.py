97f3a17084c15ac0d6f43e0d1b0519a1cd0f9e6d
hvgab <henrik.v.gabrielsen@gmail.com>
2019-11-05 17:24:14 +0100

    sentry + circleci

--- happenings/app/tests.py
+++ happenings/app/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.http import HttpRequest
+from django.urls import reverse
 
-# Create your tests here.
+from . import views
+
+class HomePageTests(TestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+
+class TVTests(TestCase):
+
+    def setUp(self):
+        Tag.objects.create(name='Oslo')
+        Tag.objects.create(name='Skien')
+        
+        Happening.objects.create(title='OsloEvent')
